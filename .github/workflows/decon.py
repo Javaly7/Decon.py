08579623261d1d3c3f3b3108ae3c1701264a0ead
Javaly <59543058+Javaly7@users.noreply.github.com>
2020-05-22 22:04:32 -0300

    Update decon.py

--- .github/workflows/decon.py
+++ .github/workflows/decon.py
@@ -1,21 +1,21 @@
-from termcolor import colored
+#versao sem termcolor
 import base64
 #Author: Javaly
 #Versao: 1.0
 #Enjoy!
 while True:
-    print(colored("""
+    print("""
     *   1) Encoder de base64
     *   2) Decoder de base64
     *   Bom dia!
-    """, 'green'))
+    """)
     try:
-        quest = int(input(colored("Escolha uma Opcao: ", 'green')))
+        quest = int(input("Escolha uma Opcao: "))
         if(quest == 1):
-            quest2 = str(input(colored("Digite o Texto: ", 'green')))
+            quest2 = str(input("Digite o Texto: "))
             print(base64.b64encode(quest2.encode('ascii')))
         elif(quest == 2):
-            quest3 = str(input(colored("Digite o Texto: ", 'green')))
+            quest3 = str(input("Digite o Texto: "))
             print(base64.b64decode(quest3.encode('ascii')))
         else:
           print("opçao invalida")
